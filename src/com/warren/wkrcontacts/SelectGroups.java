312802010e63de21a3c0e80ddd9d5ddf79eb3590
wataylo <warrensjunk@hotmail.com>
2013-02-21 22:24:43 -0600

    Working Checkboxes with CustomAdapter

--- src/com/warren/wkrcontacts/SelectGroups.java
+++ src/com/warren/wkrcontacts/SelectGroups.java
@@ -1,51 +1,47 @@
 package com.warren.wkrcontacts;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.CheckBox;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class SelectGroups extends Activity {
 
+	LayoutInflater inflater;
 	private final static String TAG = "WorkContactsActivitySelectGroups"; 
+	private CustomGroupAdapter adapter;
+	private Cursor GroupCur;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    	
+    	inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_groups);
         ListView Groups = (ListView) findViewById(R.id.groupsLv);
-        Cursor GroupCur = getAllGroups();
-        Toast t = Toast.makeText(this,"GroupCount " + GroupCur.getCount(),Toast.LENGTH_SHORT);
-        t.show();
-//        ArrayList<String> GroupArray = getUnique(GroupCur, null);
-//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.groupselectlayout , groupCur, fieldsT, to);
+        GroupCur = getAllGroups();
         String[] fieldsT = new String[] {
                 ContactsContract.Groups.TITLE
         };
         int[] to = new int[] {R.id.groupNameChk};
-        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.groupselectlayout, GroupCur, fieldsT, to);
+        adapter = new CustomGroupAdapter(this, R.layout.groupselectlayout, GroupCur, fieldsT, to);
         Groups.setAdapter(adapter);
         
-        Groups.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-        	@Override
-        	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-        		Log.v(TAG,"GroupClicked");
-        		CheckBox GroupChk = (CheckBox) findViewById(R.id.groupNameChk);
-        		
-       	}
-        });
-        	   
     }
 
     private Cursor getAllGroups() {
@@ -68,5 +64,71 @@ public class SelectGroups extends Activity {
         getMenuInflater().inflate(R.menu.activity_select_groups, menu);
         return true;
     }
-   
+
+	
+	private class CustomGroupAdapter extends SimpleCursorAdapter{
+		public boolean[] checkState;
+		ViewHolder viewholder;
+
+		public CustomGroupAdapter(Context context, int layout, Cursor c, String[] from, int[] to) {
+			super(context, layout, c, from, to);
+			checkState = new boolean[c.getCount()];
+		}
+		
+
+
+		//class for caching the views in a row  
+		 private class ViewHolder
+		 {
+		   CheckBox checkBox;
+		 }
+	 
+	   
+	 
+		 @Override
+		 public View getView(final int position, View convertView, ViewGroup parent) {
+		 
+		   if(convertView==null)
+		    {
+		   convertView=inflater.inflate(R.layout.groupselectlayout, null);
+		   viewholder=new ViewHolder();
+		 
+		    //cache the views
+		    viewholder.checkBox=(CheckBox) convertView.findViewById(R.id.groupNameChk);
+		 
+		     //link the cached views to the convertview
+		    convertView.setTag(viewholder);
+		  }
+		  else
+			   viewholder=(ViewHolder) convertView.getTag();
+			 
+			            
+			//set the data to be displayed
+		   	GroupCur.moveToPosition(position);
+			  viewholder.checkBox.setText(GroupCur.getString(1));
+			    
+			   //VITAL PART!!! Set the state of the 
+			   //CheckBox using the boolean array
+			  viewholder.checkBox.setChecked(checkState[position]);
+			     
+			  
+			   //for managing the state of the boolean
+			   //array according to the state of the
+			   //CheckBox 
+			   viewholder.checkBox.setOnClickListener(new View.OnClickListener() {
+		     
+				   public void onClick(View v) {
+				    if(((CheckBox)v).isChecked())
+				     checkState[position]=true;
+				    else
+				     checkState[position]=false;
+				      
+				    }
+			   });
+			   
+			   //return the view to be displayed
+			   return convertView;
+		  }
+		 
+	}
 }
